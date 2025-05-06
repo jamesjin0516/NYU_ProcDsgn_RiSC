b03e01fdb5a69faa28d2fa8dd748780a94bcb3f7
Ge(James) Jin <jamesjin0516@gmail.com>
2025-05-05 21:04:29 -0400

    ALU initial implementation

---
+++ ALU.v
@@ -0,0 +1,25 @@
+module ALU (
+    input [15:0] alu_src1,
+    input [15:0] alu_src2,
+    input ADD,
+    input NAND,
+    input PASS1,
+    input EQ,
+    output [15:0] alu_out,
+    output eq_out
+);
+
+    assign eq_out = (alu_src1 == alu_src2) ? 1'b1 : 1'b0;
+
+    always @(*) begin
+        if (ADD)
+            alu_out = alu_src1 + alu_src2;
+        else if (NAND)
+            alu_out = ~(alu_src1 & alu_src2);
+        else if (PASS1)
+            alu_out = alu_src1;
+        else
+            alu_out = 16'b0;
+    end
+
+endmodule
